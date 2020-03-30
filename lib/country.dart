bc2148b3fb77c43992db9f256af308ac0a4fbcfb
Chafroud Tarek <chafroudtarek6@gmail.com>
2020-03-30 22:41:41 +0100

    Covid-19

---
+++ lib/country.dart
@@ -0,0 +1,12 @@
+class Country {
+  final String name, totalCases, deaths, recovered;
+
+   Country ({
+     this.name,
+     this.totalCases,
+     this.deaths,
+     this.recovered,
+   });
+
+
+}
---
+++ lib/helper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+
+final blackBlue = Color(0xFF8b0000);
+final lightBlackBlue = Color(0x88050719);
+final purple = Color(0xFF000000);
+final lightPurple = Color(0x881A2252);
+final cyan = Color(0xFFFFFFFF);
+final lightCyan = Color(0x88FFFFF3);
